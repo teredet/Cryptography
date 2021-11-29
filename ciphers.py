077fea6e4702e542c31490fd649d9a467b9ca26e
teredet <artem.vanzytler@gmail.com>
2021-11-29 13:27:28 +0200

    Add RU alphabet for Caesar Cipher. Add Vigener Cipher.

--- ciphers.py
+++ ciphers.py
@@ -8,13 +8,20 @@ def сaesar_сiplet(menu, message, key):
     Returns a string.\n
     
     '''
-    
-    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabetENG = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabetRU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
     message = message.upper()
     key = int(key)
     output = ''
     
+    if message[0] in alphabetENG:
+        alphabet = alphabetENG
+    elif message[0] in alphabetRU:
+        alphabet = alphabetRU
+    else:
+        return 'Invalid message'
+
     if menu == 'D':
         key *= -1
     for letter in message:
@@ -25,3 +32,23 @@ def сaesar_сiplet(menu, message, key):
         else:
             output += letter
     return output
+
+def vigenere_cipher(mode, message, key):
+	'''
+    mode - 'E' if you want to encrypt and 'D' if you want to decrypt\n
+    message - the string to encode or decore\n
+    key - codeword\n
+    \n
+    The function encodes or decodes the message.\n
+    Returns a string.\n
+	
+	'''
+	key *= len(message) // len(key) + 1  
+	finalMessage = ""
+	for i, j in enumerate(message):
+		if mode == 'E':
+			temp = ord(j) + ord(key[i])
+		else:
+			temp = ord(j) - ord(key[i])
+		finalMessage += chr(temp % 26 + ord('A'))
+	return finalMessage
